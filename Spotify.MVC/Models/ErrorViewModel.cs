967babf156157484613355c645edd8f322d3686b
DylanMinda <dylant0909@gmail.com>
2025-07-08 16:55:07 -0500

    Login y registro terminado. Envio de correo de recuperacion de contraseña, enviando una contraseña temporal. Modificaciones en los controladores y servicios. Eliminacion de algunas vistas inecesarias. Creacion de planes conectados con la base de datos. Una vista general tras logearse(dashboard).

--- Spotify.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Spotify.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
